0fbda221cd10f5ae674eab79ea0be87a26e56777
sunlei <guizaicn@gmail.com>
2025-11-29 18:43:06 +0800

    ci: init

---
+++ scripts/ci/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# https://tdlib.github.io/td/build.html?language=Python
+
+cd td
+rm -rf build
+mkdir build
+
+CXXFLAGS="-stdlib=libc++" \
+    CC=/usr/bin/clang-18 \
+    CXX=/usr/bin/clang++-18 \
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX:PATH=../tdlib \
+        -DTD_ENABLE_LTO=ON \
+        -DCMAKE_AR=/usr/bin/llvm-ar-18 \
+        -DCMAKE_NM=/usr/bin/llvm-nm-18 \
+        -DCMAKE_OBJDUMP=/usr/bin/llvm-objdump-18 \
+        -DCMAKE_RANLIB=/usr/bin/llvm-ranlib-18
+
+cmake --build ./build/ --target install
+
+tree ./tdlib
